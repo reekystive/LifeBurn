0143ea7ae6c36d07fbd8e3cfeb20137d858eb4ce
Terry Deng <deng1193291298@icloud.com>
2024-03-06 16:24:37 +1300

    feat: add app interface & icon

--- LifeBurn/ContentView.swift
+++ LifeBurn/ContentView.swift
@@ -5,57 +5,31 @@
 //  Created by ReekyStive on 2024-03-05.
 //
 
-import SwiftData
 import SwiftUI
+import WidgetKit
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
+    @State private var birthDate = Date()
+    // World average, this can be adjusted.
+    @State private var lifeExpectancy = 82
 
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
+        VStack {
+            DatePicker("Enter your birth date", selection: $birthDate, displayedComponents: .date)
+                .datePickerStyle(GraphicalDatePickerStyle())
+                .padding()
+
+            Button("Set Life Progress") {
+                calculateLifeProgress(birthDate: birthDate, lifeExpectancy: lifeExpectancy)
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
         }
     }
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
+    func calculateLifeProgress(birthDate: Date, lifeExpectancy: Int) {
+        // TODO: Implement this function
     }
 }
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
 }
---
+++ LifeBurn/WidgetExtension.swift
@@ -0,0 +1,8 @@
+//
+//  WidgetExtension.swift
+//  LifeBurn
+//
+//  Created by Terry on 2024/3/5.
+//
+
+import Foundation
