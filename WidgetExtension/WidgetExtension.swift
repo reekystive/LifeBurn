6b02f87c59953268a824fe1507d4b00ea9974a6e
Mingxuan Wang <reekystive@gmail.com>
2024-03-06 14:00:51 +0800

    feat: add widget extension design

--- WidgetExtension/WidgetExtension.swift
+++ WidgetExtension/WidgetExtension.swift
@@ -5,8 +5,8 @@
 //  Created by ReekyStive on 2024-03-06.
 //
 
-import WidgetKit
 import SwiftUI
+import WidgetKit
 
 struct Provider: AppIntentTimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
@@ -16,7 +16,7 @@ struct Provider: AppIntentTimelineProvider {
     func snapshot(for configuration: ConfigurationAppIntent, in context: Context) async -> SimpleEntry {
         SimpleEntry(date: Date(), configuration: configuration)
     }
-    
+
     func timeline(for configuration: ConfigurationAppIntent, in context: Context) async -> Timeline<SimpleEntry> {
         var entries: [SimpleEntry] = []
 
@@ -37,16 +37,59 @@ struct SimpleEntry: TimelineEntry {
     let configuration: ConfigurationAppIntent
 }
 
-struct WidgetExtensionEntryView : View {
+struct WidgetExtensionEntryView: View {
     var entry: Provider.Entry
 
     var body: some View {
         VStack {
-            Text("Time:")
-            Text(entry.date, style: .time)
+            HStack {
+                Text(entry.configuration.favoriteEmoji)
+                    .font(.largeTitle)
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Est. death date")
+                        .fontDesign(.rounded)
+                        .font(.headline)
+                        .fontWeight(.medium)
+                        .foregroundColor(Color(red: 1, green: 0.89, blue: 0.89))
+                    Text("Jun 15, 2069")
+                        .fontDesign(.rounded)
+                        .font(.title2)
+                        .fontWeight(.bold)
+                }
+            }
 
-            Text("Favorite Emoji:")
-            Text(entry.configuration.favoriteEmoji)
+            Spacer()
+
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("29%")
+                        .fontDesign(.rounded)
+                        .font(.title2)
+                        .fontWeight(.bold)
+                    Text("14,348,008 minutes spent")
+                        .fontDesign(.rounded)
+                        .font(.subheadline)
+                        .fontWeight(.medium)
+                }
+                Spacer()
+            }
+
+            GeometryReader { metrics in
+                ZStack(alignment: .leading) {
+                    Rectangle()
+                        .foregroundColor(.clear)
+                        .frame(height: 8)
+                        .background(Color(red: 0.3, green: 0.3, blue: 0.29).opacity(0.5))
+
+                    Rectangle()
+                        .foregroundColor(.clear)
+                        .frame(width: metrics.size.width * 0.29, height: 8)
+                        .background(.white)
+                        .cornerRadius(8)
+                }
+                .cornerRadius(8)
+            }
         }
     }
 }
@@ -57,18 +100,28 @@ struct WidgetExtension: Widget {
     var body: some WidgetConfiguration {
         AppIntentConfiguration(kind: kind, intent: ConfigurationAppIntent.self, provider: Provider()) { entry in
             WidgetExtensionEntryView(entry: entry)
-                .containerBackground(.fill.tertiary, for: .widget)
+                .containerBackground(for: .widget) {
+                    LinearGradient(
+                        stops: [
+                            Gradient.Stop(color: Color(red: 0.96, green: 0.37, blue: 0.37), location: 0.00),
+                            Gradient.Stop(color: Color(red: 0.78, green: 0, blue: 0.61), location: 1.00),
+                        ],
+                        startPoint: UnitPoint(x: 0.5, y: 0),
+                        endPoint: UnitPoint(x: 0.5, y: 1)
+                    )
+                }
+                .foregroundColor(.white)
         }
     }
 }
 
 extension ConfigurationAppIntent {
-    fileprivate static var smiley: ConfigurationAppIntent {
+    fileprivate static var fire: ConfigurationAppIntent {
         let intent = ConfigurationAppIntent()
-        intent.favoriteEmoji = "😀"
+        intent.favoriteEmoji = "🔥"
         return intent
     }
-    
+
     fileprivate static var starEyes: ConfigurationAppIntent {
         let intent = ConfigurationAppIntent()
         intent.favoriteEmoji = "🤩"
@@ -76,9 +129,9 @@ extension ConfigurationAppIntent {
     }
 }
 
-#Preview(as: .systemSmall) {
+#Preview(as: .systemMedium) {
     WidgetExtension()
 } timeline: {
-    SimpleEntry(date: .now, configuration: .smiley)
+    SimpleEntry(date: .now, configuration: .fire)
     SimpleEntry(date: .now, configuration: .starEyes)
 }
